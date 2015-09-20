d7c687270c0ab35b4d1cb32aeec1267f0414cc38
James P. Torre, IV <torreaiworks01@gmail.com>
2015-09-20 20:30:30 +0000

    Wire behavior works.

--- 3453.scm
+++ 3453.scm
@@ -3,11 +3,11 @@
 
 (define (rlem3453)
 	(letrec* ([id 0]
-						[role 'stem]  ;;stem gate wire
+						[role 'stem]            ;;  stem gate wire
 						[mem 0]
-						[buf '()]  ;;stem cell msg buffer
-						[terms '(0 0 0 0 0 0)]  ;;A/C' B/A' C/B'
-						[nbrs '(0 0 0)]  ;;A/C' B/A' C/B'
+						[buf '()]               ;;  stem cell msg buffer
+						[terms '(0 0 0 0 0 0)]  ;;  A B C A' B' C'
+						[nbrs '(0 0 0)]         ;;  B/A' C/B' A/C'
 						[state `((id . ,id) (role . ,role) (mem . ,mem) (buf . ,buf) 
 										 (terms . ,terms) (nbrs . ,nbrs))]
 						[state-change
@@ -24,51 +24,95 @@
 															 (buf . ,buf) (terms . ,terms) 
 															 (nbrs . ,nbrs)))
 								 ))]
-						[act (lambda ()
-									 (case role
-										 ['wire `(,id . wire)]
-;										 ['wire (transfer terms nbrs)]
-										 [else `(,id . no-action)]
-										 ))]
-
-						#;						[act (lambda ()							
-						(case role
-						['wire (begin
-						(transfer terms nbrs)
-						(state-change 'terms '(0 0 0 0 0 0)))]
-						['gate (begin
-						(transfer terms nbrs)
-						(state-change 'terms '(0 0 0 0 0 0))
-						(if (eq? mem 0)
-						(state-change 'mem 1)
-						(state-change 'mem 0)))]
-						))]
-						[transfer (lambda (sig des)
-												(cond
-												 [(eq? '(1 0 0 0 0 0) sig)
-													(((cadr des) 'set) 'terms '(0 0 0 1 0 0))]
-												 [(eq? '(0 0 0 0 0 0) sig) 'no-transfer]))]
+						[act 
+						 (lambda ()
+							 (cond
+								['wire (begin (transfer) (transmit) (reset)
+															)]
+								[else `(,id . no-action)]
+								))]
+						[transfer
+						 (lambda ()
+							 (cond
+								[(eq? '(0 0 0 0 0 0) terms) 'no-transfer]
+								[(eq? mem 0)  ;;  turn right
+								 (state-change
+									'terms (append (list-tail terms 3)
+																 (list-head terms 3)))]  ;;  ABC->A'B'C'
+								[(eq? mem 1)  ;;  turn left
+								 (state-change
+									'terms (append '(0 0 0) (list 
+																					 (list-ref terms 2)       ;;  C->A'
+																					 (list-ref terms 0)       ;;  A->B'
+																					 (list-ref terms 1))))]   ;;  B->C'
+								[else 'bad-transfer]
+								))]
+						[transmit 
+						 (lambda ()
+							 (cond
+								[(> (list-index terms 1) 2)
+								 (let* ([target (list-ref nbrs (- (list-index terms 1) 3))]
+												[new-terms (append (list (list-ref terms 5)
+																								 (list-ref terms 3)
+																								 (list-ref terms 4))
+																					 '(0 0 0))])
+									 (target `(set terms ,new-terms)))]
+								[else 'bad-transmit]
+								))]
+						[reset
+						 (lambda ()
+							 (state-change 'terms '(0 0 0 0 0 0))
+							 )]
 						)
 					 (lambda (msg)
 						 (cond
-							[(assoc msg state) (assoc msg state)]
+							[(assoc msg state) (assoc msg state)]  ;;  report state element
+							[(eq? msg 'state) state]							 ;;  report all state
+							[(eq? msg 'step) (act)]
 							[(eq? (car msg) 'set)
 							 (let ([o (cadr msg)]
-										 [n (cddr msg)])
+										 [n (caddr msg)])
 								 (begin
 									 (state-change o n)
 									 ))]
-							[(eq? (car msg) 'step) (act)]
 							[else 'ill-msg]))))
 
+(define r0 (rlem3453))
 (define r1 (rlem3453))
 (define r2 (rlem3453))
+(define r3 (rlem3453))
+
+(define (disp-all-state)
+	(begin
+		(display (r0 'state))	(newline)
+		(display (r1 'state))	(newline)
+		(display (r2 'state))	(newline)
+		(display (r3 'state))	(newline)
+		))
 
 (define (test)
 	(begin
-		(r2 '(set id 1))
-		(r1 '(set nbrs (0 r2 0)))
-		(r1 '(set role wire))
-		(r1 '(set terms (1 0 0 0 0 0)))
+		(display "Tabula rasa") (newline)
+		  (disp-all-state)
+		(r1 '(set id 1))
+		(r2 '(set id 2))
+		(r3 '(set id 3))
+		(r0 `(set nbrs (,r1 ,r2 ,r3)))  ;;  B/A'->r1 C/B'->r2 A/C'->r3
+		(r0 '(set role wire))
+		(r0 '(set terms (1 0 0 0 0 0)))  ;;  term A
+		(display "Turn right config") (newline)
+  		(disp-all-state)
+		(r0 'step)		
+		(display "Turn right result") (newline)
+	  	(disp-all-state)
+		(r0 '(set mem 1))
+		(r0 '(set terms (0 1 0 0 0 0)))  ;;  term B
+		(display "Turn left config") (newline)
+		  (disp-all-state)
+		(r0 'step)
+    (display "Turn left result") (newline)
+		  (disp-all-state)
 		)
 )
+
+(test)
