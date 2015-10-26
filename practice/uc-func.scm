e4f5715b21272ceb5984d939b754dee36b5274d4
James P. Torre, IV <torreaiworks01@gmail.com>
2015-10-26 23:53:54 +0000

    UTC20151026

--- practice/uc-func.scm
+++ practice/uc-func.scm
@@ -2,17 +2,30 @@
 ;;jpt4
 ;;UTC20151024
 
-(define (rlem3453 state symbol)
-	(case state
+(define (rlem3453 state)
+	(define mem state)
+  (define (switch)
+		
+	
+	(define (self msg)
+		(case 
+			(case state
 		['r (case symbol
 					['a 'a
 		['l (rotate -1 symbol)]))
 
+				self)
 (define (rotate distance sequence)
 	(let ([len (length sequence)]
-				[piv (mod (
+
 				[dist (abs distance)])
 		(if (eq? (* (abs distance) 2) len)
 				(list (list-tail sequence (- dist 1)) (list-head sequence (- dist 1)))
-				
-	 
+				)))
+
+
+;;DEL = Q x SIG -> Q x GAM
+(define (rlem3453 mem sym)
+	
+	
+					
