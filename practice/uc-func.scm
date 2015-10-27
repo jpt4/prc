b293939262ed8c85f2c8c4448578acf3ee053477
James P. Torre, IV <torreaiworks01@gmail.com>
2015-10-27 23:57:01 +0000

    rlem3453 with no output symbol interpretation

--- practice/uc-func.scm
+++ practice/uc-func.scm
@@ -2,6 +2,7 @@
 ;;jpt4
 ;;UTC20151024
 
+#|
 (define (rlem3453 state)
 	(define mem state)
   (define (switch)
@@ -22,10 +23,22 @@
 		(if (eq? (* (abs distance) 2) len)
 				(list (list-tail sequence (- dist 1)) (list-head sequence (- dist 1)))
 				)))
-
+|#
 
 ;;DEL = Q x SIG -> Q x GAM
 (define (rlem3453 mem sym)
-	
-	
+	(cond
+	 [(eq? mem 0) (cons 1 (cons (case sym
+																['0 4]
+																['1 5]
+																['2 3])
+															'()))]
+	 [(eq? mem 1) (cons 0 (cons (case sym
+																['0 5]
+																['1 3]
+																['2 4])
+															'()))]
+	 ))
+
+(define (lattice-node) 'ln)
 					
