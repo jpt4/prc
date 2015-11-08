555c7ede23b31312fa2a2ebd2ebb354dd441ccf0
James P. Torre, IV <torreaiworks01@gmail.com>
2015-11-08 23:34:37 +0000

    Rethink symbols and targets

--- practice/uc-func.scm
+++ practice/uc-func.scm
@@ -2,31 +2,18 @@
 ;;jpt4
 ;;UTC20151024
 
-#|
-(define (rlem3453 state)
-	(define mem state)
-  (define (switch)
-		
-	
-	(define (self msg)
-		(case 
-			(case state
-		['r (case symbol
-					['a 'a
-		['l (rotate -1 symbol)]))
+;;(rlem-X) functions represent the change in UC state individually,
+;;and cannot be trivially composed without extra-systemically
+;;accounting for interconnection within a UC matrix; thus, they are
+;;insufficiently thorough logical specifications. The formal
+;;foundation of a PRC machine is a matrix of UCs, each atomically
+;;primitive, but also indivisibly a part of a whole.
 
-				self)
-(define (rotate distance sequence)
-	(let ([len (length sequence)]
-
-				[dist (abs distance)])
-		(if (eq? (* (abs distance) 2) len)
-				(list (list-tail sequence (- dist 1)) (list-head sequence (- dist 1)))
-				)))
-|#
+;;TODO 0 is self, 1,2,3 are A,B,C terminals. Change numbering to
+;;reflect.
+;;Define different sym-rotate for rlem-buf
 
 ;;DEL = Q x SIG -> Q x GAM
-
 (define (rlem3453-base input)
 	(rlem3453-base-aux input))
 
@@ -71,6 +58,7 @@
 
 (define max-buf 5)
 (define null-sym '_)
+(define null-buf '())
 (define stem-commands (list 'zero 'one 'to-log 'to-wir))
 
 ;;STATE = MEMORY x ROLE x BUFFER x HIGH-RAIL x LOW-RAIL
@@ -80,12 +68,12 @@
 				 [buf (cadddr input)] [sig '(list-ref input 4)])
 		(case rol
 			['stm (cond
-						 [(or (member? sym sig) (member? sym stem-commands))
+						 [(or (member? sym sig) (member? (cdr sym) stem-commands))
 									(buf-parse mem sym rol (buf-modify sym buf sig) sig)]
 						 [(list mem '_ rol buf (sig-update sym sig))]
 						 )]
 			['wir (cond
-						 [(eq? sym 'stem-init) (list mem null-sym 'stm '() sig)]
+						 [(eq? (cadr sym) 'stem-init) (list mem null-sym 'stm '() sig)]
 						 [(list mem (sym-rotate mem sym) rol buf sig)]
 						 )]
 			['log (cond
@@ -102,12 +90,14 @@
 
 ;;send message to target, according to buffer value
 (define (buf-parse mem sym rol buf sig)
-	(let* ([tar (list-head buf 2)] [msg (list-tail buf 2)]
-				 [act (case msg
-								['(0 0 0) '(mem zero  ])
-							])
-		(case tar
-			['(0 0) 
+	(let* ([tar (list-bin->dec (list-head buf 2))] 
+				 [msg (list-tail buf 2)])
+		(case msg 
+			['(0 0 0) (list mem `(,tar . zero) rol null-buf sig)]
+			['(0 0 1) (list mem `(,tar . one) rol null-buf sig)]
+			['(0 1 0) (list mem `(,tar . to-log) rol null-buf sig)]			
+			['(0 1 1) (list mem `(,tar . to-wir) rol null-buf sig)]			
+			['(1 0 0) (list mem `(,tar . stem-init) rol null-buf sig)])))		
 
 ;;update which entry terminals are high and low signal rails
 ;;sig = (low high)
