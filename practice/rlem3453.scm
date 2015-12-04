b95cac72bb200864de505e2120459303193f3e42
James P. Torre, IV <torreaiworks01@gmail.com>
2015-12-04 23:55:19 +0000

    UTC20151204

---
+++ practice/rlem3453.scm
@@ -0,0 +1,23 @@
+;rlem3453.scm
+;RLEM 3-453 as function, circuits thereof
+;jpt4
+;UTC20151204
+
+(define (rlem3453 inp)
+	(let* ([mem (car inp)]
+				 [sym (cadr inp)]
+				 [new-mem (abs (- mem 1))]
+				 [new-sym	(case mem
+										['0 (rotate sym 1)]
+										['1 (rotate sym 2)])]
+				 [out (cons new-mem (cons new-sym '()))]
+				 )
+		out)
+)
+
+(define (rotate ls num)
+	(let* ([snum (modulo num (length ls))] ;sanitized shift value
+				 [new-head (list-tail ls snum)]
+				 [new-tail (list-head ls snum)])
+		(append new-head new-tail)))
+		
