939ebafb8772ce2cb0ffdc800edbf5c8a92ea695
James P. Torre, IV <torreaiworks01@gmail.com>
2015-12-06 21:22:58 +0000

    UTC20151206

--- practice/rlem3453.scm
+++ practice/rlem3453.scm
@@ -8,14 +8,14 @@
 				 [sym (cadr inp)]
 				 [new-mem (abs (- mem 1))]
 				 [new-sym	(case mem
-										['0 (rotate sym 1)]
-										['1 (rotate sym 2)])]
+										['0 (list-rotate sym 1)]
+										['1 (list-rotate sym 2)])]
 				 [out (cons new-mem (cons new-sym '()))]
 				 )
 		out)
 )
 
-(define (rotate ls num)
+(define (list-rotate ls num)
 	(let* ([snum (modulo num (length ls))] ;sanitized shift value
 				 [new-head (list-tail ls (- (length ls) snum))]
 				 [new-tail (list-head ls (- (length ls) snum))])
