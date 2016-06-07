3f863c01c25e526f63ceb67d838e3c2312fb9a2a
James P. Torre, IV <torreaiworks01@gmail.com>
2016-06-07 19:02:38 +0000

    Stem transistion, not blocked by existing output.

--- practice/asmsim.scm
+++ practice/asmsim.scm
@@ -65,55 +65,66 @@
 ;;state machine interpretation
 (define (step asm)
   (match asm
-    [($ <uc-asm> 'q0 (? empty pre) 
+    ;;wire standard signal
+    [($ <uc-asm> 'q0 (? empty pre) ;0 0 0 -> 0 0 0
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? empty inp) (? empty out)))
      asm]
-    [($ <uc-asm> 'q0 (? empty pre) 
+    [($ <uc-asm> 'q0 (? empty pre) ;0 0 1ne -> 0 0 1ne
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? empty inp) (? not-empty out)))
      asm]
-    [($ <uc-asm> 'q0 (? empty pre) 
+    [($ <uc-asm> 'q0 (? empty pre) ;0 1ss 0 -> 0 0 1ss
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? standard inp) (? empty out)))
      (set-fields asm 
-                 [(asm-core? inp?) '()] 
+                 [(asm-core? inp?) (clear inp)]
                  [(asm-core? out?) (rotate inp (+ 1 mem))])]
-    [($ <uc-asm> 'q0 (? empty pre) 
+    [($ <uc-asm> 'q0 (? empty pre) ;0 1ss 1ne -> 0 1ss 1ne
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? standard inp) (? not-empty out)))
      asm]
-    [($ <uc-asm> 'q0 (? not-empty pre) 
+    [($ <uc-asm> 'q0 (? not-empty pre) ;1ne 0 0 -> 0 1ne 0
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? empty inp) (? empty out)))
      (set-fields asm 
-                 [(pre?) '()]
+                 [(pre?) (clear pre)]
                  [(asm-core? inp?) pre])]
-    [($ <uc-asm> 'q0 (? not-empty pre) 
+    [($ <uc-asm> 'q0 (? not-empty pre) ;1ne 0 1ne -> 0 1ne 1ne 
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? empty inp) (? not-empty out)))
      (set-fields asm 
-                 [(pre?) '()]
+                 [(pre?) (clear pre)]
                  [(asm-core? inp?) pre])]
-    [($ <uc-asm> 'q0 (? not-empty pre) 
+    [($ <uc-asm> 'q0 (? not-empty pre) ;1ne 1ss 0 -> 1ne 0 1ss
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? standard inp) (? empty out)))
      (set-fields asm 
-                 [(pre?) '()]
-                 [(asm-core? inp?) pre]
+                 [(asm-core? inp?) (clear inp)]
                  [(asm-core? out?) (rotate inp (+ 1 mem))])]
-    [($ <uc-asm> 'q0 (? not-empty pre) 
+    [($ <uc-asm> 'q0 (? not-empty pre) ;1ne 1ss 1ne -> 1ne 1ss 1ne
         ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
            (? standard inp) (? not-empty out)))
      asm]
+    ;;wire special signal
+    [($ <uc-asm> 'q0 pre ;wire: 0/1 1sm 0/1 -> stem: 0 0 0
+        ($ <uc-core> 'wire mem (? empty hig) (? empty buf) (? empty smb)
+           (? stem-init? inp) out))
+     (set-fields asm
+                 [(asm-core? rol?) 'stem] [(asm-core? mem?) 0] 
+                 [(asm-core? inp?) (clear inp)] [(asm-core? out?) (clear out)]
+                 )]
     [_ 'halt]
     ))
 
-(define (empty v) (null? v))
-(define (not-empty v) (not (null? v)))
+(define (empty v) (and-map (lambda (l) (eq? l '_)) v))
+(define (not-empty v) (not (empty v)))
 (define (standard i) 
   (and (not (eq? '(_ _ _) i))
        (map (lambda (a) (or (eq? a 1) (eq? a 0) (eq? a '_))) i)))
+(define (special? i) (exactly? 1 (lambda (a) (member? a special-messages)) i))
+(define (stem-init? i) (exactly? 1 (lambda (a) (eq? a 'stem-init)) i))
+(define (clear f) (map (lambda (a) '_) f))
 (define (rotate l n) (list-rotate l n))
 (define (list-rotate ls num)
 	(let* ([snum (modulo num (length ls))] ;sanitized shift value
@@ -136,8 +147,8 @@
 (define (dispnl* tls) 
   (cond
    [(and (pair? tls) (pair? (cdr tls))) 
-    (begin (for-each dispnl* (car tls)) (for-each dispnl* (cdr tls)))]
-   [(pair? tls) (for-each dispnl* (car tls))]
+    (begin (dispnl* (car tls)) (for-each dispnl* (cdr tls)))]
+   [(and (pair? tls) (null? (cdr tls))) (dispnl* (car tls))]
    [else (dispnl tls)]))
    
 
@@ -156,52 +167,69 @@
                   [(asm-core? mem?) '1]
                   [(sta?) 'init])
       "0 0 0"
-      (let ([asm (mk-uc-asm 'q0 '() 
-                            (mk-uc-core 'wire 0 '() '() '() '() '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(_ _ _) (mk-uc-core 
+                                'wire 0 '() '() '() '(_ _ _) '(_ _ _)))])
         (list asm (step asm)))
       "0 0 1"
-      (let ([asm (mk-uc-asm 'q0 '() 
-                            (mk-uc-core 'wire 0 '() '() '() '() '(1 0 1)))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(_ _ _) (mk-uc-core 
+                                'wire 0 '() '() '() '(_ _ _) '(1 0 1)))])
         (list asm (step asm)))
       "0 1sr 0"
-      (let ([asm (mk-uc-asm 'q0 '() 
-                            (mk-uc-core 'wire 0 '() '() '() '(1 0 1) '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(_ _ _) (mk-uc-core 
+                                'wire 0 '() '() '() '(1 0 1) '(_ _ _)))])
         (list asm (step asm)))
       "0 1sl 0"
-      (let ([asm (mk-uc-asm 'q0 '() 
-                            (mk-uc-core 'wire 1 '() '() '() '(1 0 1) '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(_ _ _) (mk-uc-core 
+                                'wire 1 '() '() '() '(1 0 1) '(_ _ _)))])
         (list asm (step asm)))
       "0 1ns 0"
-      (let ([asm (mk-uc-asm 'q0 '() 
-                            (mk-uc-core 'wire 1 '() '() '() '(_ _ _) '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(_ _ _) (mk-uc-core 
+                                'wire 1 '() '() '() '(_ _ _) '(_ _ _)))])
         (list asm (step asm)))
       "0 1 1"
-      (let ([asm (mk-uc-asm 'q0 '() 
-                            (mk-uc-core 'wire 0 '() '() '() '(1 0 1) '(0 0 _)))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(_ _ _) (mk-uc-core 
+                                'wire 0 '() '() '() '(1 0 1) '(0 0 _)))])
         (list asm (step asm)))
       "1 0 0"
-      (let ([asm (mk-uc-asm 'q0 '(1 _ 0) 
-                            (mk-uc-core 'wire 0 '() '() '() '() '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core 
+                                'wire 0 '() '() '() '(_ _ _) '(_ _ _)))])
         (list asm (step asm)))
       "1 0 1"
-      (let ([asm (mk-uc-asm 'q0 '(1 _ 0) 
-                            (mk-uc-core 'wire 0 '() '() '() '() '(1 1 1)))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core 
+                                'wire 0 '() '() '() '(_ _ _) '(1 1 1)))])
         (list asm (step asm)))
       "1 1sr 0"
-      (let ([asm (mk-uc-asm 'q0 '(1 _ 0) 
-                            (mk-uc-core 'wire 0 '() '() '() '(_ 1 1) '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core
+                                'wire 0 '() '() '() '(_ 1 1) '(_ _ _)))])
         (list asm (step asm)))
       "1 1sl 0"
-      (let ([asm (mk-uc-asm 'q0 '(1 _ 0) 
-                            (mk-uc-core 'wire 1 '() '() '() '(_ 1 1) '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core 
+                                'wire 1 '() '() '() '(_ 1 1) '(_ _ _)))])
         (list asm (step asm)))
       "1 1ns 0"
-      (let ([asm (mk-uc-asm 'q0 '(1 _ 0) 
-                            (mk-uc-core 'wire 1 '() '() '() '(_ _ _) '()))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core 
+                                'wire 1 '() '() '() '(_ _ _) '(_ _ _)))])
         (list asm (step asm)))
       "1 1 1"
-      (let ([asm (mk-uc-asm 'q0 '(1 _ 0) 
-                            (mk-uc-core 'wire 0 '() '() '() '(1 1 1) '(0 0 0)))])
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core 
+                                'wire 0 '() '() '() '(1 1 1) '(0 0 0)))])
         (list asm (step asm)))
+      (let ([asm (mk-uc-asm 
+                  'q0 '(1 _ 0) (mk-uc-core 
+                                'wire 0 '() '() '() '(1 stem-init 1) 
+                                '(0 1 0)))])
+        (list asm (step asm)))      
       ))           
     ))
