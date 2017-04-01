7c1f1d2f30ef1bffb46385082544c896393663cc
James P. Torre, IV <torreaiworks01@gmail.com>
2017-04-01 23:57:10 +0000

    A1

--- practice/asmsim.scm
+++ practice/asmsim.scm
@@ -46,15 +46,32 @@
             (halt asm)]
            [(qw0 ,i ,o ,m) (guard (empty? o))
             (begin
-              (sta! asm qw1)
+              (sta! asm 'qw1)
               (wire-step asm))]
            [(qw1 ,u ,i empty ,m) (guard (empty? u))
             (halt asm)]
            [(qw1 ,u ,i empty ,m) (guard (not-empty? u))
             (begin
-              (inp! asm u) (ups! asm empty)
+              (ups! asm empty) (inp! asm u)
               (wire-step asm))]
-;;;...
+           [(qw2 empty ,i empty ,m) (guard? (contains-stem-init? i))
+            (begin
+              (sta! asm 'qw3)
+              (typ! asm 'stem) (inp! asm empty) (mem! asm right) 
+              (aut! asm empty) (con! asm empty) (buf! asm empty)
+              (halt asm))] ;;collapse qw3 due to asm/asm-data mismatch
+           [(qw2 empty ,i empty ,m) (guard? (all-standard-signals? i))
+            (begin
+              (sta! asm qw4)
+              (wire-step asm))]
+           [(qw4 empty ,i empty ,m) 
+            (guard? (all-standard-signals? i)) (guard? (equals? right m))
+            (begin
+              (sta! asm qw5)
+              (inp! asm empty) (out! asm (right-rotate i))
+                    
+                                                      
+           
 )))
                       
            
