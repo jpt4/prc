043b5b678f4e1458b856c9ac285c7a1db171c218
James P. Torre, IV <torreaiworks01@gmail.com>
2017-04-02 01:29:30 +0000

    Monolith vs. decomposed.

--- practice/asmsim.scm
+++ practice/asmsim.scm
@@ -38,13 +38,14 @@
               (stem-step asm))]
            )))
 
+;;monolithic matching
 (define (wire-step asm)
   (let ([asm-data (list
                    (sta asm) (ups asm) (inp asm) (out asm) (mem asm))])
     (match asm-data
            [(qw0 ,u ,i ,o ,m) (guard (not-empty? o))
             (halt asm)]
-           [(qw0 ,i ,o ,m) (guard (empty? o))
+           [(qw0 ,u ,i ,o ,m) (guard (empty? o))
             (begin
               (sta! asm 'qw1)
               (wire-step asm))]
@@ -64,15 +65,80 @@
             (begin
               (sta! asm qw4)
               (wire-step asm))]
-           [(qw4 empty ,i empty ,m) 
-            (guard? (all-standard-signals? i)) (guard? (equals? right m))
+           [(qw4 empty ,i empty ,m) (guard? (equals? right m))
             (begin
-              (sta! asm qw5)
-              (inp! asm empty) (out! asm (right-rotate i))
-                    
-                                                      
-           
+              (sta! asm 'qw5) (inp! asm empty) (out! asm (right-rotate i))
+              (wire-step asm))]
+           [(qw5 empty empty ,o right)
+            (halt asm)]
+           [(qw4 empty ,i empty ,m) (guard? (equals? left m))
+            (begin
+              (sta! asm 'qw6) (inp! asm empty) (out! asm (left-rotate i))
+              (wire-step asm))]
+           [(qw6 empty empty ,o left)
+            (halt asm)]           
 )))
+
+;;dispatch to individual functions
+(define (proc asm)
+  (let ([asm-data (list
+                   (ups asm) (inp asm) (out asm) (mem asm))])
+    (qp0 asm asm-data)))
+(define (qp0 asm asm-data)  
+    (match asm-data
+           [(,u ,i ,o ,m) (guard (not-empty? o))
+            (halt asm)]
+           [(,u ,i ,o ,m) (guard (empty? o))
+            (qp1 asm asm-data)])))
+(define (qp1 asm asm-data);;?asm-data a thunk, call per state to get new data?
+  (let ([asm-data (list
+                   (ups asm) (inp asm) (out asm) (mem asm))])
+    (match asm-data
+           [(,u ,i empty ,m) (guard (empty? u))
+            (halt asm)]
+           [(,u ,i empty ,m) (guard (not-empty? u))
+            (begin
+              (ups! asm empty) (inp! asm u)
+              (qp2 asm))])))
+(define (qp2 asm)
+  (let ([asm-data (list
+                   (ups asm) (inp asm) (out asm) (mem asm))])
+    (match asm-data
+           [(empty ,i empty ,m) (guard? (contains-stem-init? i))
+            (begin
+              (typ! asm 'stem) (inp! asm empty) (mem! asm right) 
+              (aut! asm empty) (con! asm empty) (buf! asm empty)
+              (qp3 asm))] ;no need to collapse qw3 due to per state asm-data
+           [(empty ,i empty ,m) (guard? (all-standard-signals? i))
+            (qp4 asm)])))           
+(define (qp3 asm)
+  (match asm
+         [(stem empty empty empty right empty empty empty)
+          (halt asm)]))
+(define (qp4 asm)
+  (let ([asm-data (list
+                   (ups asm) (inp asm) (out asm) (mem asm))])
+    (match asm-data
+           [(empty ,i empty ,m) (guard? (equals? right m))
+            (begin
+              (inp! asm empty) (out! asm (right-rotate i))
+              (qp5 asm))]
+           [(empty ,i empty ,m) (guard? (equals? left m))
+            (begin
+              (inp! asm empty) (out! asm (left-rotate i))
+              (qp6 asm))])))
+(define (qp5 asm)
+  (let ([asm-data (list
+                   (ups asm) (inp asm) (out asm) (mem asm))])
+    (match asm-data  
+           [(empty empty ,o right)
+            (halt asm)])))
+(define (qp6 asm)
+  (let ([asm-data (list
+                   (ups asm) (inp asm) (out asm) (mem asm))])
+    (match asm-data  
+           [(empty empty ,o left)
+            (halt asm)])))
                       
            
            
